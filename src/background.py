e49d04e030fe8e199a95fa2e4f05514a12d63eff
Brianna O <98538246+briannasexton@users.noreply.github.com>
2022-04-21 13:56:48 +0000

    create controller class and classes

---
+++ src/background.py
@@ -0,0 +1,11 @@
+import pygame
+from src.utility import Utility
+
+class Background:
+
+#display score
+#display snake boundries
+#display snake
+#display apples
+
+#create folder for all images
---
+++ src/snake.py
@@ -0,0 +1,9 @@
+import pygame
+from src.utility import Utility
+
+class Snake:
+  
+
+#import image file name
+
+#location of snake (x,y)
