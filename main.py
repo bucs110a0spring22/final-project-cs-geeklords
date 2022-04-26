e54f3a988c5db1824e9cd5c37382078ea629ae60
Julia D D <98531822+juliasteckler@users.noreply.github.com>
2022-04-26 02:45:55 +0000

    Added class outlines

--- main.py
+++ main.py
@@ -15,6 +15,30 @@ print(mylist)
   
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
+class Snake:
+  """Directs snake up, down, left, right, and controls color, speed, growth, actions
+  """
+  def __init__(self, x, y, filename):
+    self.x = x
+    self.y = y
+    self.image = str(filename)
+
+class Apples:
+  """Positions apples and keeps track of score
+  """
+  def innit(self, x, y, filename, score):
+    self.x = x
+    self.y = y
+    self.image = str(filename)
+    se.f.score = 
+    
+class Background:
+  """Displays user score, creates snake's boundaries, displays snake, displays fruit
+  """
+  def __init__(self, filename):
+    self.image = str(filename)
+
+  
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
